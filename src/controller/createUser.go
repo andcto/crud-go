0c338b2a3ed25a241b439f8554dc44f2780df119
Andrey <andreyc3@gmail.com>
2023-10-07 20:30:48 -0300

    configurando logger

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,15 +1,18 @@
 package controller
 
 import (
+	"github.com/andcto/crud-go/src/configuration/logger"
 	"github.com/andcto/crud-go/src/configuration/validation"
 	"github.com/andcto/crud-go/src/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err)
 		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
