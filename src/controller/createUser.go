c9fc2702d9de540bde9cd2862707682603a868a7
Andrey <andreyc3@gmail.com>
2023-10-07 19:23:53 -0300

    configuração de validação create user

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,7 @@
 package controller
 
 import (
-	"fmt"
-
-	"github.com/andcto/crud-go/src/configuration/rest_err"
+	"github.com/andcto/crud-go/src/configuration/validation"
 	"github.com/andcto/crud-go/src/model/request"
 	"github.com/gin-gonic/gin"
 )
@@ -12,8 +10,7 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
